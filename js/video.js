93dd32c1ede77131e95df06d381a602ac8747033
FILIPRAIDER <filipraider123@gmail.com>
2023-09-07 21:39:39 -0500

    cambio de prueba

--- js/video.js
+++
@@ -1,8 +0,0 @@
-const video = document.getElementById("video");
-const play = document.getElementById("play");
-
-play.addEventListener("click", function () {
-    
-  video.play();
-  video.controls=false;
-});
